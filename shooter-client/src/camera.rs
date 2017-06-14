b8217775a0c394ce4b42fd5fa70bbf2bcd20846d
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2017-06-14 18:14:46 +0200

    added camera.rs and entity.rs

---
+++ shooter-client/src/camera.rs
@@ -0,0 +1,5 @@
+use super::na::
+
+pub struct Camera {
+
+}
---
+++ shooter-client/src/entity.rs
@@ -0,0 +1,16 @@
+use super::transform::*;
+use super::mesh::*;
+
+pub struct Entity {
+    transform: Transform,
+    mesh: Mesh,
+}
+
+impl Entity {
+    pub fn new_sprite() -> Entity {
+        Entity {
+            transform: Transform::new(),
+            mesh: Mesh::create_quad,
+        }
+    }
+}
