54c3d6ec02047cc9ba0d5301cbafb0079f345c02
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2018-03-27 15:16:34 +0200

    Loading obj

--- shooter-client/src/mesh/mod.rs
+++ shooter-client/src/mesh/mod.rs
@@ -4,3 +4,6 @@ pub mod mesh;
 use na::core::Vector3;
 
 pub type Vertex<T> = Vector3<T>;
+
+#[derive(Debug)]
+pub struct Face(i32,i32,i32);
--- shooter-client/src/mesh/model.rs
+++ shooter-client/src/mesh/model.rs
@@ -2,16 +2,21 @@ extern crate wavefront_obj;
 
 use self::wavefront_obj::obj;
 use utils::file::read_asset;
-use super::Vertex;
-use std::io::Error;
+use super::{Vertex,Face};
 
-pub struct Object {
+#[derive(Debug)]
+pub enum Shape {
+    Triangle(Face),
+}
+
+pub struct Geometry {
+    shapes: Vec<Shape>,
 }
 
 pub struct Model {
     name: String,
     vertices: Vec<Vertex<f64>>,
-    //objects: Vec<Object>,
+    geometry: Vec<Geometry>,
 }
 
 impl Model {
@@ -21,8 +26,18 @@ impl Model {
         if let Ok(obj) = obj::parse(content) {
             let ret = obj.objects.iter().map(|o| {
                 Model {
-                    name: o.name.clone(),
-                    vertices: o.vertices.iter().map(|v| Vertex::new(v.x, v.y, v.z)).collect()
+                    name: o.name.to_string(),
+                    vertices: o.vertices.iter().map(|v| Vertex::new(v.x, v.y, v.z)).collect(),
+                    geometry: o.geometry.iter().map(|g| Geometry {
+                        shapes: g.shapes.iter().map(|s| {
+                            match s.primitive {
+                                self::wavefront_obj::obj::Primitive::Triangle(a,b,c) => {
+                                    Shape::Triangle(Face(a.0 as _, b.0 as _, c.0 as _))
+                                },
+                                _ => { panic!("Unsupported shape primitive") }
+                            }
+                        }).collect()
+                    }).collect()
                 }
             }).collect();
             Ok(ret)
@@ -38,8 +53,17 @@ mod tests {
     
     #[test]
     fn load_al_model() {
-        if let Ok(model) = Model::load_from_wavefront_file("al.obj") {
-            
+        if let Ok(models) = Model::load_from_wavefront_file("al.obj") {
+            /*println!("Models:");
+            for m in  models {
+                println!("\tModel name : {}", m.name);
+                for g in m.geometry {
+                    println!("\t\tShape:");
+                    for t in g.shapes {
+                        println!("\t\t\tTriangle: {:?}", t)
+                    }
+                }
+        }*/
         } else {
             panic!("Model did not load correctly")
         }
